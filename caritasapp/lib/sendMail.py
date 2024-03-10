ac2e5a9effa6f7d8d4afe1808e8bb2bd7c8ddb9b
ashwin202 <ashes192000@gmail.com>
2024-03-10 14:43:57 +0530

    Add changes to senMail

--- caritasapp/lib/sendMail.py
+++ caritasapp/lib/sendMail.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from datetime import datetime
 
 
-def sendMail(from_email, to_email, message, subject):
-    to_email = "ericjohn26296@gmail.com"
+def sendMail(from_email, to_email, message, subject, is_enquire_form = False):
+    # to_email = "ericjohn26296@gmail.com"
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     try:
         # Manually configure Django settings
@@ -19,7 +19,11 @@ def sendMail(from_email, to_email, message, subject):
                 EMAIL_HOST_PASSWORD="teyi ntre ujro rdch",
                 DEFAULT_FROM_EMAIL="ashes192000@gmail.com",
             )
-        send_mail(subject, message, from_email, [to_email])
+        if is_enquire_form:
+            to_email_list = [to_email, "managerbandc@caritashospital.org"]
+        else:
+            to_email_list = [to_email]
+        send_mail(subject, message, from_email, to_email_list)
 
         print(
             f"[{timestamp} | sendMail] | From: {from_email} | Email sent successfully!"
